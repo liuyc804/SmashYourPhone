f4a569e27bd425e5ef6260dab2af68427e0d6881
liuyc804 <35980618+liuyc804@users.noreply.github.com>
2019-12-01 01:02:21 -0500

    Create theme, app bar icons, app bar design.

--- app/src/main/java/umd/cmsc434/smashyourphone/HomeActivity.java
+++ app/src/main/java/umd/cmsc434/smashyourphone/HomeActivity.java
@@ -1,6 +1,7 @@
 package umd.cmsc434.smashyourphone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,6 @@ public class HomeActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		
 	}
 }
