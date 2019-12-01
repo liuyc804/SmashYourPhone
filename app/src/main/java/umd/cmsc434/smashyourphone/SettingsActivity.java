e8a93cd19bf15d1dcc0ba44537e59895953062a1
liuyc804 <35980618+liuyc804@users.noreply.github.com>
2019-12-01 17:12:39 -0500

    Create home interface. Create app logo. Make changes in app bar design.

--- app/src/main/java/umd/cmsc434/smashyourphone/SettingsActivity.java
+++ app/src/main/java/umd/cmsc434/smashyourphone/SettingsActivity.java
@@ -12,20 +12,6 @@ public class SettingsActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
-		getSupportFragmentManager()
-						.beginTransaction()
-						.replace(R.id.settings, new SettingsFragment())
-						.commit();
-		ActionBar actionBar = getSupportActionBar();
-		if (actionBar != null) {
-			actionBar.setDisplayHomeAsUpEnabled(true);
-		}
 	}
 	
-	public static class SettingsFragment extends PreferenceFragmentCompat {
-		@Override
-		public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-			setPreferencesFromResource(R.xml.root_preferences, rootKey);
-		}
-	}
 }
--- app/src/main/java/umd/cmsc434/smashyourphone/ShareActivity.java
+++ app/src/main/java/umd/cmsc434/smashyourphone/ShareActivity.java
@@ -11,4 +11,5 @@ public class ShareActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_share);
 	}
+	
 }
